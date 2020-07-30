da67482dbc4bb82a8e9d53817605bc8795d419fd
Kenenbek R <kenenbek@gmail.com>
2020-07-30 16:47:23 +0300

    add data generate function

--- roerich/__init__.py
+++ roerich/__init__.py
@@ -1,5 +1,6 @@
 from .algorithms import CLF, RuLSIF
+from .dataset import generate_dataset
 
 __all__ = [
-    'CLF', 'RuLSIF'
+    'CLF', 'RuLSIF', 'generate_dataset'
 ]
--- roerich/dataset.py
+++ roerich/dataset.py
@@ -17,6 +17,24 @@ import numpy as np
 import pandas as pd
 
 
+def generate_dataset(period=200, N_tot=1000):
+    mu = 0
+    sigma = 1.
+    N = 1
+    
+    T = [0, 1]
+    X = [np.random.normal(mu, sigma, 1)[0], np.random.normal(mu, sigma, 1)[0]]
+    
+    for i in range(2, N_tot):
+        if i % period == 0:
+            N += 1
+            mu += 0.5 * N
+        T += [i]
+        ax = 0.6 * X[i-1] - 0.5 * X[i-2] + np.random.normal(mu, sigma, 1)[0]
+        X += [ax]
+    return np.array(X).reshape(-1, 1)
+
+
 class TimeSeries:
     def __init__(
         self,
