8f4a719206ed4ee15e703126be57e63d607200ac
Kenenbek R <kenenbek@gmail.com>
2020-07-30 17:14:43 +0300

    return labels from dataset

--- roerich/dataset.py
+++ roerich/dataset.py
@@ -24,15 +24,17 @@ def generate_dataset(period=200, N_tot=1000):
     
     T = [0, 1]
     X = [np.random.normal(mu, sigma, 1)[0], np.random.normal(mu, sigma, 1)[0]]
+    label = np.zeros(N_tot)
     
     for i in range(2, N_tot):
         if i % period == 0:
             N += 1
             mu += 0.5 * N
+            label[i] = 1
         T += [i]
         ax = 0.6 * X[i-1] - 0.5 * X[i-2] + np.random.normal(mu, sigma, 1)[0]
         X += [ax]
-    return np.array(X).reshape(-1, 1)
+    return np.array(X).reshape(-1, 1), label
 
 
 class TimeSeries:
