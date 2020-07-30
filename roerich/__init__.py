75728ef4e20bbb004cf8c2d44ccb917da49ee54a
Kenenbek R <kenenbek@gmail.com>
2020-07-30 17:06:24 +0300

    import viz changed

--- roerich/__init__.py
+++ roerich/__init__.py
@@ -1,6 +1,8 @@
 from .algorithms import CLF, RuLSIF
 from .dataset import generate_dataset
+from .viz import display
+
 
 __all__ = [
-    'CLF', 'RuLSIF', 'generate_dataset'
+    'CLF', 'RuLSIF', 'generate_dataset', 'display'
 ]
