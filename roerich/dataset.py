83be56f3ddabf3a22183db9c7728ccb0e7cc43d7
hushchyn-mikhail <hushchyn.mikhail@gmailcom>
2022-06-22 09:55:55 +0300

    range fix

--- roerich/dataset.py
+++ roerich/dataset.py
@@ -1,133 +1,22 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
-Example code for loading a dataset to a TimeSeries object.
-
-Note that this code requires Pandas to be available.
-
-Author: Gertjan van den Burg
-Copyright: The Alan Turing Institute, 2019
-License: See LICENSE file.
-
-"""
-
-import json
 import numpy as np
-import pandas as pd
+
 
 
 def generate_dataset(period=200, N_tot=1000):
     mu = 0
     sigma = 1.
     N = 1
-    
+
     T = [0, 1]
     X = [np.random.normal(mu, sigma, 1)[0], np.random.normal(mu, sigma, 1)[0]]
-    label = np.zeros(N_tot)
-    
+    cps = []
+
     for i in range(2, N_tot):
         if i % period == 0:
             N += 1
             mu += 0.5 * N
-            label[i] = 1
+            cps.append(i)
         T += [i]
         ax = 0.6 * X[i-1] - 0.5 * X[i-2] + np.random.normal(mu, sigma, 1)[0]
         X += [ax]
-    return np.array(X).reshape(-1, 1), label
-
-
-class TimeSeries:
-    def __init__(
-        self,
-        t,
-        y,
-        name=None,
-        longname=None,
-        datestr=None,
-        datefmt=None,
-        columns=None,
-    ):
-        self.t = t
-        self.y = y
-
-        self.name = name
-        self.longname = longname
-        self.datestr = datestr
-        self.datefmt = datefmt
-        self.columns = columns
-
-        # whether the series is stored as zero-based or one-based
-        self.zero_based = True
-
-    @property
-    def n_obs(self):
-        return len(self.t)
-
-    @property
-    def n_dim(self):
-        return self.y.shape[1]
-
-    @property
-    def shape(self):
-        return (self.n_obs, self.n_dim)
-
-    @classmethod
-    def from_json(cls, filename):
-        with open(filename, "rb") as fp:
-            data = json.load(fp)
-
-        tidx = np.array(data["time"]["index"])
-        tidx = np.squeeze(tidx)
-
-        if "format" in data["time"]:
-            datefmt = data["time"]["format"]
-            datestr = np.array(data["time"]["raw"])
-        else:
-            datefmt = None
-            datestr = None
-
-        y = np.zeros((data["n_obs"], data["n_dim"]))
-        columns = []
-
-        for idx, series in enumerate(data["series"]):
-            columns.append(series.get("label", "V%i" % (idx + 1)))
-            thetype = np.int if series["type"] == "integer" else np.float64
-            vec = np.array(series["raw"], dtype=thetype)
-            y[:, idx] = vec
-
-        ts = cls(
-            tidx,
-            y,
-            name=data["name"],
-            longname=data["longname"],
-            datefmt=datefmt,
-            datestr=datestr,
-            columns=columns,
-        )
-        return ts
-
-    @property
-    def df(self):
-        d = {"t": self.t}
-        for i in range(len(self.columns)):
-            col = self.columns[i]
-            val = self.y[:, i]
-            d[col] = val
-        return pd.DataFrame(d)
-
-    def make_one_based(self):
-        """ Convert the time index to a one-based time index. """
-        if self.zero_based:
-            self.t = [t + 1 for t in self.t]
-            self.zero_based = False
-
-    def __repr__(self):
-        return "TimeSeries(name=%s, n_obs=%s, n_dim=%s)" % (
-            self.name,
-            self.n_obs,
-            self.n_dim,
-        )
-
-    def __str__(self):
-        return repr(self)
+    return np.array(X).reshape(-1, 1), cps
--- roerich/viz.py
+++ roerich/viz.py
@@ -2,44 +2,58 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def display(X, T, L, S, Ts, peaks=None, plot_peak_height=10, s_max=10):
-    n = X.shape[1] + 1 if peaks is None else X.shape[1] + 2
-    
+def display(X, cps_true, score, cps_pred=None, plot_peak_height=10, s_max=10):
+
+    T = np.arange(len(X))
+    L = np.zeros(len(X))
+    L[cps_true] = 1
+
+    n = X.shape[1] + 1 if cps_pred is None else X.shape[1] + 2
+
     plt.figure(figsize=(12, n*2.5+0.25))
-    
+
     for i in range(X.shape[1]):
-        
-        plt.subplot(n, 1, i+1)
+
         ax = X[:, i]
+        d = 0.05 * (ax.max() - ax.min())
+
+        plt.subplot(n, 1, i+1)
         plt.plot(T, ax, linewidth=2, label='Original signal', color='C0')
         for t in T[L == 1]:
-            plt.plot([t]*2, [ax.min(), ax.max()], color='0', linestyle='--')
-        plt.ylim(ax.min(), ax.max())
+            plt.plot([t]*2, [ax.min()-d, ax.max()+d], color='0', linestyle='--')
+        plt.ylim(ax.min()-d, ax.max()+d)
         plt.xlim(0, T.max())
         plt.xticks(size=16)
         plt.yticks(size=16)
         plt.legend(loc='upper left', fontsize=16)
         plt.tight_layout()
-    
-    score_plot_ix = n if peaks is None else n - 1
+
+    score_plot_ix = n if cps_pred is None else n - 1
+    d = 0.05 * (score.max() - score.min())
     plt.subplot(n, 1, score_plot_ix)
-    plt.plot(Ts, S, linewidth=3, label="Change-point score", color='C3')
+    plt.plot(T, score, linewidth=3, label="Change point score", color='C3')
     for t in T[L == 1]:
-        plt.plot([t]*2, [-1, s_max], color='0', linestyle='--')
-    
-    # display find peaks #todo refactoring
-    if peaks is not None:
-        plt.subplot(n, 1, n)
-        new_score_peaks = np.zeros(len(T))
-        new_score_peaks[peaks] = plot_peak_height
-        plt.plot(new_score_peaks, linewidth=3, label="Peaks", color='C4')
-        for t in T[L == 1]:
-            plt.plot([t]*2, [-1, s_max], color='0', linestyle='--')
-    
-    plt.ylim(-1, s_max)
+        plt.plot([t]*2, [score.min()-d, score.max()+d], color='0', linestyle='--')
+    plt.ylim(score.min()-d, score.max()+d)
     plt.xlim(0, T.max())
     plt.xticks(size=16)
-    plt.yticks(np.arange(0, s_max+1, 5), size=16)
-    plt.xlabel("Time", size=16)
+    plt.yticks(size=16)
     plt.legend(loc='upper left', fontsize=16)
+    plt.tight_layout()
+
+    # display find peaks #todo refactoring
+    if cps_pred is not None:
+        plt.subplot(n, 1, n)
+        L_pred = np.zeros(len(T))
+        L_pred[cps_pred] = 1
+        plt.plot(L_pred, linewidth=3, label="Change points", color='C4')
+        for t in T[L == 1]:
+            plt.plot([t]*2, [-0.05, 1.05], color='0', linestyle='--')
+        plt.ylim(-0.05, 1.05)
+        plt.xlim(0, T.max())
+        plt.xticks(size=16)
+        plt.yticks(size=16)
+        plt.legend(loc='upper left', fontsize=16)
+
+    plt.xlabel("Time", size=16)
     plt.tight_layout()
